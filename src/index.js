d942a20d23ca57f620d4eb8c50b73d0cfb92d838
Akshay Kumar <akshaykhigh8285@gmail.com>
2023-08-25 07:17:43 +0530

    added auth logic

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "App";
-import store from "one-infotech/redux/store";
 import { Provider } from "react-redux";
-
+import { persistor, store } from "./one-infotech/redux/store";
+import { PersistGate } from "redux-persist/integration/react";
 // Material Dashboard 2 PRO React Context Provider
 import { MaterialUIControllerProvider } from "context";
 
@@ -15,7 +15,9 @@ root.render(
   <BrowserRouter>
     <MaterialUIControllerProvider>
       <Provider store={store}>
-        <App />
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
       </Provider>
     </MaterialUIControllerProvider>
   </BrowserRouter>
