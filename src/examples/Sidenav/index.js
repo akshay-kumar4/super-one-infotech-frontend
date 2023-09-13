35147e213d350e5dbd5f3532b2abb1e387d87aff
Gauranshi Gupta <95520163+ggauranshi-03@users.noreply.github.com>
2023-09-13 16:36:31 +0530

    logo image

--- src/examples/Sidenav/index.js
+++ src/examples/Sidenav/index.js
@@ -278,11 +278,15 @@ function Sidenav({ color, brand, brandName, routes, ...rest }) {
           </MDTypography>
         </MDBox>
         <MDBox component={NavLink} to="/" display="flex" alignItems="center">
-          {/* {brand && <MDBox component="img" src={brand} alt="Brand" width="2rem" />} */}
+          {/* {brand && <MDBox component="img" src={"/public/S.png"} alt="Brand" width="2rem" />} */}
+          {/* <img src="/S.png" alt="Brand" width="85vm" />
+          <br /> */}
           <MDBox
             width={!brandName && "100%"}
             sx={(theme) => sidenavLogoLabel(theme, { miniSidenav })}
           >
+            <img src="/S.png" alt="Brand" width="85vm" />
+            <br />
             <MDTypography
               component="h1"
               variant="button"
